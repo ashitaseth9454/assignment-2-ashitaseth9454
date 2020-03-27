23f983993ba8cacab0d1bbad2fc8551f6cfd5db4
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 01:32:47 +0530

    constructing main method

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -12,14 +12,14 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree mbst = new MyBinarySearchTree();
-        mbst.insert(95);
+        mbst.insert(85);
         mbst.insert(35);
         mbst.insert(40);
         mbst.insert(16);
         mbst.insert(21);
         mbst.insert(10);
-        mbst.insert(2);
         mbst.insert(21);
+        mbst.insert(2);
         mbst.display(mbst.getRoot(), mbst.getM());
         mbst.newDisplay();
 
