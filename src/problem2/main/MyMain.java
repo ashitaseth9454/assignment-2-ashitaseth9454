e81eabedef20eeb8616b0ca19ccc3e0671a23465
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 01:49:44 +0530

    made the post and pre method for checking preOrder traversal and postOrder traversal

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,8 +5,9 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
-
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
@@ -20,9 +21,29 @@ public class MyMain {
         mbst.insert(21);
         mbst.insert(10);
         System.out.println("root" + mbst.getRoot().getData());
-        System.out.println("preorder traversal");
+        System.out.println("pre-order traversal");
+        MyMain mm = new MyMain();
+        mm.pre(mbst.getRoot());
+        System.out.println();
+        System.out.println("post order traversal");
+        mm.post(mbst.getRoot());
+        System.out.println("a is yes");
+        System.out.println("b is no");
+    }
 
+    public void pre(TreeNode node) {
+        if (node != null) {
+            System.out.println(node.getData());
+            pre(node.getLeft());
+            pre(node.getRight());
+        }
+    }
 
+    public void post(TreeNode node) {
+        if (node != null) {
+            post(node.getLeft());
+            post(node.getRight());
+        }
 
 
 
