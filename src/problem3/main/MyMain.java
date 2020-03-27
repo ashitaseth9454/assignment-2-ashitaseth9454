bba6ebf916d68c4809ff041a70e8c77db2c6413d
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 02:44:34 +0530

    adding name and rollnumbers and displaying them

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,10 +5,17 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
-
+        MyPriorityQueue mpq = new MyPriorityQueue();
+        mpq.push("ashi", 1);
+        mpq.push("anshika", 2);
+        mpq.push("anamika", 3);
+        mpq.display();
     }
 }
