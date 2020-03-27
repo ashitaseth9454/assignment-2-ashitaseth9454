d35a1f7c07803eb073d14524102f51b40c8a2d33
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 00:12:17 +0530

    construction method for insertion

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -18,6 +18,27 @@ public class MyBinarySearchTree {
     }
 
     public void insert(int data) {
+        TreeNode newnode = new TreeNode(data);
+        if (root == null) {
+            root = newnode;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (newnode.getData() >= temp.getData()) {
+                    temp = temp.getRight();
+                } else {
+                    temp = temp.getLeft();
+                }
+                if (newnode.getData() <= parent.getData()) {
+                    parent.setLeft(newnode);
+                } else {
+                    parent.setRight(newnode);
+                }
+            }
+        }
+
 
     }
 }
