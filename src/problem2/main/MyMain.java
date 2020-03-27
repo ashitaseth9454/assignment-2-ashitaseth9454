895ec48e55deb227fe29e2216aecd5d06e7383c5
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 01:36:08 +0530

    inserting values in the tree

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,10 +5,24 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree mbst = new MyBinarySearchTree();
+        mbst.insert(85);
+        mbst.insert(35);
+        mbst.insert(40);
+        mbst.insert(16);
+        mbst.insert(21);
+        mbst.insert(10);
+
+
+
+
 
     }
 }
