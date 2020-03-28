1224f97c9cb2573f05a13c7cfff7164072184785
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 12:52:44 +0530

    constructed insert method

--- src/problem4/binarySearchTree/MyBinarySearchTree.java
+++ src/problem4/binarySearchTree/MyBinarySearchTree.java
@@ -13,6 +13,25 @@ public class MyBinarySearchTree {
 
     public void insert(int data) {
         TreeNode node = new TreeNode(data);
+        if (root == null) {
+            root = node;
+        } else {
+            TreeNode current = root;
+            TreeNode parent = null;
+            while (current != null) {
+                parent = current;
+                if (node.getData() < current.getData()) {
+                    current = current.getLeft();
+                } else {
+                    current = current.getRight();
+                }
+            }
+            if (node.getData() < parent.getData()) {
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
 
 
     }
