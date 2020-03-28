0f5240199bbe1c8ecc2a6f9fcc326b73669f6783
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 12:46:39 +0530

    constructing getter and setter for root Node

---
+++ src/problem4/binarySearchTree/MyBinarySearchTree.java
@@ -0,0 +1,17 @@
+package problem4.binarySearchTree;
+
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
+public class MyBinarySearchTree {
+    private MyQueue mq;
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+}
