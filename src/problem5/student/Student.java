bd9ce8f6c2880fa9c80439cb7cf385fe1d1b39ab
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 13:07:02 +0530

    constructing getter and setter

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -11,4 +11,33 @@ public class Student {
     int backLogCounter;
     int appearingCounter;
 
+    public Student(String name, int backLogCounter, int appearingCounter) {
+        this.name = name;
+        this.backLogCounter = backLogCounter;
+        this.appearingCounter = appearingCounter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public int getAppearingCounter() {
+        return appearingCounter;
+    }
+
+    public void setAppearingCounter(int appearingCounter) {
+        this.appearingCounter = appearingCounter;
+    }
 }
