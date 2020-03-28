7e9367609a28a0c23adcd42dd0de1461120b14f0
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 13:20:50 +0530

    constructed insert method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,18 +6,34 @@
  */
 package problem5.circularqueue;
 
-import problem4.linkedlist.Node;
+import problem5.node.Node;
 import problem5.student.Student;
 
 //to implement circular queue
 public class MyCircularQueue {
     Node front, rear;
 
+
     public MyCircularQueue() {
         front = rear = null;
     }
 
     public void insert(Student stu) {
+        if (stu.getAppearingCounter() > 2) {
+            System.out.println(stu.getName() + " cannot insert");
+        } else {
+            Node node = new Node(stu);
+            if (front == null) {
+                front = node;
+            } else {
+                rear.setNext(node);
+                rear = node;
+                rear.setNext(front);
+            }
+
+        }
+
 
     }
+
 }
