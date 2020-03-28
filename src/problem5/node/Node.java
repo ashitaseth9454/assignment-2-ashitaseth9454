9a40decbf4546aba9a5daca1b941e199bccbe091
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 13:08:55 +0530

    constructing getter and setter

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -5,6 +5,32 @@
  *  Time: 7:06 PM
  */
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    Student stu;
+    Node next;
+
+    public Node(Student stu) {
+        this.stu = stu;
+        this.next = null;
+    }
+
+    public Student getStu() {
+        return stu;
+    }
+
+    public void setStu(Student stu) {
+        this.stu = stu;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
