7867821064418576ae287ad8ef9948eb00e04f89
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-03-28 13:12:23 +0530

    constructing insert method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,19 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem4.linkedlist.Node;
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+    Node front, rear;
+
+    public MyCircularQueue() {
+        front = rear = null;
+    }
+
+    public void insert(Student stu) {
+
+    }
 }
